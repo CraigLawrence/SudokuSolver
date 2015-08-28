038f95a0ae20fb89e3396ebeb4758d0ecc3e58bb
Craig Lawrence <craig@live.com.au>
2015-08-28 18:53:20 +0930

    Added comments to One Possible Value strategy.

--- src/sudoku/solve/StrategyOnePossibleValue.java
+++ src/sudoku/solve/StrategyOnePossibleValue.java
@@ -8,7 +8,7 @@ import sudoku.model.Cell;
 import sudoku.model.CellGroup;
 
 /*
- * This strategy checks the possible values for unassigned cell,
+ * This strategy checks the possible values for each unassigned cell,
  * and if there is one possible value, assigns it.
  */
 
@@ -21,10 +21,13 @@ public class StrategyOnePossibleValue implements Strategy {
 		boolean changed = false;
 		
 		Set<Character> possibleValues;
+		// Consider each cell
 		for (CellGroup cellgroup : board.getCellGroups().values()) {
 			for (Cell cell : cellgroup.getCells()) {
+				// Assign a new value if the cell is empty
 				if (cell.getValue() == board.getEmptyValue()){
 					possibleValues = cell.possibleValues();
+					// Assign a value if there's only one possibility
 					if (possibleValues.size() == 1){
 						changed = true;
 						cell.changeValue((char) possibleValues.toArray()[0]);
@@ -33,6 +36,7 @@ public class StrategyOnePossibleValue implements Strategy {
 			}
 		}
 		
+		// Add new board to pool
 		if (changed)
 			branches.add(board);		
 		return branches;
